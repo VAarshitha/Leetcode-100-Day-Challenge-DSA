048fd0f715db82e358b39b01aea30bcec6ce9faf
VAarshitha <150131931+VAarshitha@users.noreply.github.com>
2025-03-11 18:09:41 +0530

    Time: 2 ms (98.83%), Space: 45.2 MB (29.67%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,25 +1,13 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Create a map to store numbers and their indices
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
-        // Iterate through the array of numbers
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];  // Correct complement calculation
-            
-            // Check if complement exists in the map
-            if (map.containsKey(complement)) {
-                return new int[]{map.get(complement), i};  // Return indices of the two numbers
-            }
-            
-            // Store the current number and its index in the map
-            map.put(nums[i], i);
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+        int complement=target-nums[i];
+        if(map.containsKey(complement)){
+            return new  int[]{i,map.get(complement)} ;
         }
-        
-        // If no solution is found, throw an exception
-        throw new IllegalArgumentException("No valid two sum solution");
+        map.put(nums[i],i);
+        }
+     return  new int[]{};
     }
 }
